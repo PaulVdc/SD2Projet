9c5bee0d8d55b38d9c479564a992073c8e456b6d
paul.vandecasteele <paul.vandecasteele@student.vinci.be>
2025-03-14 14:44:43 +0100

    init

---
+++ src/Graph.java
@@ -0,0 +1,11 @@
+public class Graph {
+
+  public Graph(String file, String file1) {
+  }
+
+  public void trouverCheminLePlusCourt(String theBeatles, String kendjiGirac) {
+  }
+
+  public void trouverCheminMaxMentions(String theBeatles, String kendjiGirac) {
+  }
+}
