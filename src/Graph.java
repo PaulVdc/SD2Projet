9aa6def2513f09b7b15028ae469beddbdefc12e9
paul.vandecasteele <paul.vandecasteele@student.vinci.be>
2025-03-20 22:03:41 +0100

    modif collection artiste pour get en O1

--- src/Graph.java
+++ src/Graph.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Graph {
 
-    private static Map<Integer, Artist> artists;// retient tout les artistes
+    /*private static Map<Integer, Artist> artists;*/// retient tout les artistes
+    private static Map<String, Artist> artists;// retient tout les artistes avec le nom comme clé// retient tout les artistes par leur nom
     private static Map<Integer, Mention> mentions;//TODO à retirer ??
 
     private Map<Artist, Map<Integer, Artist>> artistsMentionnes;//Liste d'adjacence
@@ -31,7 +33,8 @@ public class Graph {
                     Artist artiste = new Artist(id, nom, categorie);
 
                     //System.out.println(artiste); //Pour verifier si l'ajout s'est bien passé
-                    this.artists.put(artiste.getId_artist(), artiste);
+                    /*this.artists.put(artiste.getId_artist(), artiste);*/
+                    this.artists.put(artiste.getNom_artist(), artiste);
                 }
             }
         } catch (IOException e){
@@ -51,7 +54,7 @@ public class Graph {
                     // Création de l'objet Mention
                     Mention mention = new Mention(this.artists.get(id_mentionneur), this.artists.get(id_mentionne), nb_mention);
 
-                    System.out.println(mention); //Pour verifier si l'ajout s'est bien passé
+                   /* System.out.println(mention);*/ //Pour verifier si l'ajout s'est bien passé
                     //TODO ajouter la mention à this.mentions
 
                 }
@@ -65,6 +68,9 @@ public class Graph {
 
     //methodes
     public Object trouverCheminLePlusCourt(String artiste1, String artiste2){
+        Artist start = this.artists.get(artiste1);
+        Artist end = this.artists.get(artiste2);
+        System.out.println(start.toString() + end.toString());
         return null;
     }
 
